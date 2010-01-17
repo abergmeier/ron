00055aade4b17f61e88f2056d40719b1ac5fe436
LCID Fire <lcid-fire@gmx.net>
2010-01-17 12:22:10 +0100

    Move Player to PlayerImpl Make Player a Interface so we can be sure it's only created out of Database class

--- server/src/org/ron/servlet/Player.java
+++ server/src/org/ron/servlet/Player.java
@@ -1,109 +1,28 @@
 package org.ron.servlet;
 
-import java.sql.SQLException;
 import java.util.Calendar;
 
 import org.ron.PositionCollision;
 
-
-public class Player
-implements Position
+public interface Player
+extends Position
 {
-	private Integer _id;
-	private PlayerDatabase _database;
-	private String _name;
+	public Integer getId();
+	
+	public String getName();
 
-	public Player(int id, PlayerDatabase players)
-	{
-		_database = players;
-		_id = id;
-	}
+	public Position getPosition();
 	
-	@Override
-	public boolean equals(Object object)
-	{
-		if(!(object instanceof Player))
-			return false;
-		
-		return getId() == ((Player)object).getId();
-	}
+	public void setPosition(float lat, float lng);
+	
+	public boolean hasLost();
 
-	public Integer getId()
-	{
-		return _id;
-	}
-	
-	public void setId(int value)
-	{
-		_id = value;
-	}
-	
-	public String getName()
-	throws SQLException
-	{
-		if(_name == null)
-			_name = _database.getPlayerName(this);
-		
-		return _name;			
-	}
+	public float getLatitude();
 
-	public void setPosition(Position position)
-	throws SQLException
-	{
-		_database.setPosition(this, position);		
-	}
+	public float getLongitude();
 	
-	public void setPosition(float lat, float lng)
-	throws SQLException
-	{
-		_database.setPosition(this, lat, lng);
-	}
-	
-	public Position getPosition()
-	{
-		return this;
-	}
-	
-	public boolean hasLost()
-	throws SQLException
-	{
-		return _database.hasLost(this);
-	}
-
-	public float getLatitude()
-	{
-		
-		try
-		{
-			return _database.getLatitude(this);
-		}
-		catch (SQLException e)
-		{
-			throw new IllegalStateException(e);
-		}
-	}
-
-	public float getLongitude()
-	{
-		try
-		{
-			return _database.getLongitude(this);
-		}
-		catch (SQLException e)
-		{
-			throw new IllegalStateException(e);
-		}
-	}
-	
-	public Segment[] getSegments(Calendar time)
-	throws SQLException
-	{
-		return _database.getNodes().getSegments().toArray(this, time);
-	}
+	public Segment[] getSegments(Calendar time);
 	
 	public void getUpdate(ClientWriter writer, Segment[] segments)
-	throws SQLException, PositionCollision
-	{
-		_database.getNodes().getSegments().getViews().getUpdate(writer, this, segments);
-	}
+	throws PositionCollision;
 }
---
+++ server/src/org/ron/servlet/PlayerImpl.java
@@ -0,0 +1,145 @@
+package org.ron.servlet;
+
+import java.sql.SQLException;
+import java.util.Calendar;
+
+import org.ron.PositionCollision;
+
+
+public class PlayerImpl
+implements Player
+{
+	private Integer _id;
+	private PlayerDatabase _database;
+	private String _name;
+
+	public PlayerImpl(int id, PlayerDatabase players)
+	{
+		_database = players;
+		_id = id;
+	}
+	
+	@Override
+	public boolean equals(Object object)
+	{
+		if(!(object instanceof Player))
+			return false;
+		
+		return getId() == ((Player)object).getId();
+	}
+
+	public Integer getId()
+	{
+		return _id;
+	}
+	
+	public void setId(int value)
+	{
+		_id = value;
+	}
+	
+	public String getName()
+	{
+		try
+		{
+			if(_name == null)
+				_name = _database.getPlayerName(this);
+			
+			return _name;			
+		}
+		catch(SQLException exception)
+		{
+			throw wrapInRuntimeException(exception);
+		}
+	}
+
+	public void setPosition(Position position)
+	throws SQLException
+	{
+		_database.setPosition(this, position);		
+	}
+	
+	public void setPosition(float lat, float lng)
+	{
+		try
+		{
+			_database.setPosition(this, lat, lng);
+		}
+		catch(SQLException exception)
+		{
+			throw wrapInRuntimeException(exception);
+		}
+	}
+	
+	public Position getPosition()
+	{
+		return this;
+	}
+	
+	public boolean hasLost()
+	{
+		try
+		{
+			return _database.hasLost(this);
+		}
+		catch(SQLException exception)
+		{
+			throw wrapInRuntimeException(exception);
+		}
+	}
+
+	public float getLatitude()
+	{
+		
+		try
+		{
+			return _database.getLatitude(this);
+		}
+		catch (SQLException e)
+		{
+			throw wrapInRuntimeException(e);
+		}
+	}
+
+	public float getLongitude()
+	{
+		try
+		{
+			return _database.getLongitude(this);
+		}
+		catch (SQLException e)
+		{
+			throw wrapInRuntimeException(e);
+		}
+	}
+	
+	public Segment[] getSegments(Calendar time)
+	{
+		try
+		{
+			return _database.getNodes().getSegments().toArray(this, time);
+		}
+		catch(SQLException exception)
+		{
+			throw wrapInRuntimeException(exception);
+		}
+	}
+	
+	public void getUpdate(ClientWriter writer, Segment[] segments)
+	throws PositionCollision
+	{
+		try
+		{
+			_database.getNodes().getSegments().getViews().getUpdate(writer, this, segments);
+		}
+		catch(SQLException exception)
+		{
+			throw wrapInRuntimeException(exception);
+		}
+	}
+	
+	private RuntimeException wrapInRuntimeException(SQLException exception)
+	{
+		return SQLiteConnection.wrapInRuntimeException(exception);
+	}
+}
