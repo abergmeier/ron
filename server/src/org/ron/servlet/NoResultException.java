2be1a69fb9f2d5fbfcd558fb25a537e8cc0a1694
LCID Fire <lcid-fire@gmx.net>
2010-01-31 11:59:43 +0100

    Solve one warning by adding a serial to NoResultException

--- server/src/org/ron/servlet/NoResultException.java
+++ server/src/org/ron/servlet/NoResultException.java
@@ -5,6 +5,8 @@ import java.sql.SQLDataException;
 public class NoResultException
 extends SQLDataException
 {
+	private static final long serialVersionUID = -5323407982815518740L;
+
 	public NoResultException(String message)
 	{
 		super(message);
